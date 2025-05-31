b4e32d218ceaea39dd85e0c81dba8ca120d3cea4
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-05-31 18:26:12 +0200

    Update beverage.py

--- beverage.py
+++ beverage.py
@@ -1,5 +1,7 @@
-from abc import ABC, abstractmethod
 class Beverage(ABC):
+    def __init__(self, size='medium'):
+        self.size = size.lower()
+
     @abstractmethod
     def get_description(self) -> str:
         pass
@@ -7,3 +9,11 @@ class Beverage(ABC):
     @abstractmethod
     def cost(self) -> float:
         pass
+
+    def get_size_multiplier(self):
+        if self.size == 'small':
+            return 0.9
+        elif self.size == 'large':
+            return 1.3
+        else:
+            return 1.0
